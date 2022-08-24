89d6ecd3270a1c2d0953258165eee0581cc6cb64
Tung <tung@.gmail.com>
2022-08-24 12:02:05 +0700

    first commit

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import './home.css';
+
+home = () => {
+    return(
+        <div className='wapper'>
+            <div className='siderbar'>
+                <h1>Trang home</h1>
+            </div>
+        </div>
+    );
+}
+
+export default home;
